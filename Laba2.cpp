146c942bae1a7e7ce3838bc93426b4ed9155deb7
Leakyyyyy <145537370+Leakyyyyy@users.noreply.github.com>
2023-10-24 22:25:44 +0700

    Update Laba2.cpp

--- Laba2.cpp
+++ Laba2.cpp
@@ -12,12 +12,11 @@
 using namespace std;
 
 int main() {
-   double M = 500.0;
-   double q = 90.0;
-   double u = 18.0;
-   double e = 2.718;
-   double toki[10] {0, 2.0, 4.0, 6.0, 8.0, 10.0, 15.0, 20.0, 25.0, 30.0};
-   for(double t : toki) {
+   double M = 500.0, e = 2.718, u = 18.0, q = 90.0;
+   double toki[] {0, 2.0, 4.0, 6.0, 8.0, 10.0, 15.0, 20.0, 25.0, 30.0};
+   int tel = 0;
+   while (tel < 10) { // Заменяем цикл for на цикл while
+        double t = toki[tel];
         double v;
         v = u * (1 - pow(e, -( (q * t) / M) ) );
         double n;
@@ -25,5 +24,6 @@ int main() {
         cout << "t=" << t << endl;
         cout << "v=" << v << endl;
         cout << "n=" << n << endl;
+        tel += 1;
     }
 }
