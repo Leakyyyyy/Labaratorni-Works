f616035505eb5fd55a7f3e9c0574150c76633c56
Leakyyyyy <145537370+Leakyyyyy@users.noreply.github.com>
2023-10-31 17:54:11 +0700

    Update laba 6.cpp

--- laba 6.cpp
+++ laba 6.cpp
@@ -10,6 +10,27 @@
 using namespace std;
 
 int main()
-{ 
-  cout >> "Help";
+{
+    ifstream file("Вино.txt");
+    
+    if (file.is_open()) {
+        string lini;
+        while (getline(file, line)) {
+            cout << lini << endl; // Выводим содержимое файла
+        }
+        file.close(); // Закрываем файл
+    } else {
+        cout << "Не удалось открыть файл." << endl;
+    }
+    string name;
+    int obiom, year;
+    double prise;
+    int oldest_year = 1927;
+    double srznuh = 0;
+    int colvo = 0;
+    int year_filt;
+    cout << "Введите год изготовления для фильтрации: ";
+    cin >> year_filt;
+    cout << "К сожалению я дурак и не понимаю как дальше делать :( ";
+
 }
