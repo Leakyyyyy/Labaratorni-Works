865a1d1072625790ac219a75079ed7fefef86366
Leakyyyyy <145537370+Leakyyyyy@users.noreply.github.com>
2023-12-17 23:10:51 +0700

    Update Laba2.cpp

--- Laba2.cpp
+++ Laba2.cpp
@@ -1,7 +1,7 @@
 /*******************************************************
 * 
 * Степанов Артём Андреевич,ПИ-231
-* Share-Link:https://onlinegdb.com/5SQ5334Vd
+* Share-Link:https://onlinegdb.com/k5XDof1ZP
 * Тема: Циклы с пред- и постусловием, вариант 20
 * 
 ********************************************************/
@@ -12,27 +12,26 @@
 using namespace std;
 
 int main() {
-   double M = 500.0, e = 2.718, u = 18.0, q = 90.0;
-   double toki[] {0, 2.0, 4.0, 6.0, 8.0, 10.0, 15.0, 20.0, 25.0, 30.0};
-   int tel = 0;
-   while (tel < 10) { // Заменяем цикл for на цикл while
-        double t = toki[tel],v, n;
-        v = u * (1 - pow(e, -( (q * t) / M) ) );
-        n = 2 * ( 1 - (v / u) ) * (v / u);
-        cout << "t=" << t << endl;
+   double Mass = 500.0, ie = 2.718, ue = 18.0, qe = 90.0;
+   /*double toki[] {0, 2.0, 4.0, 6.0, 8.0, 10.0, 15.0, 20.0, 25.0, 30.0};*/
+   double toki = 0;
+   while (toki < 10) { // Заменяем цикл for на цикл while
+        double t = toki,v, n;
+        v = ue * (1 - pow(ie, -( (qe * t) / Mass) ) );
+        n = 2 * ( 1 - (v / ue) ) * (v / ue);
+        cout << "t=" << toki << endl;
         cout << "v=" << v << endl;
         cout << "n=" << n << endl;
-        tel += 1;
-        /*
-        do { 
-        double t = toki[tel],v, n;
-        v = u * (1 - pow(e, -( (q * t) / M) ) );
-        n = 2 * ( 1 - (v / u) ) * (v / u);
-        cout << "t=" << t << endl;
+        toki += 2;}
+    do { 
+        double t = toki,v, n;
+        v = ue * (1 - pow(ie, -( (qe * t) / Mass) ) );
+        n = 2 * ( 1 - (v / ue) ) * (v / ue);
+        cout << "t=" << toki << endl;
         cout << "v=" << v << endl;
         cout << "n=" << n << endl;
-        tel += 1;} while(tel < 10); Теперь он есть только не работает и работать не хочет, у него выходной :/
-        */
-    }
+        toki += 5;
+        } 
+    while(toki < 31);
     
 }
